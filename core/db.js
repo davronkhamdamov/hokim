aa22304daeff8f6dc736847a8d65c7981bb0aa11
davronkhandamov <xamdamovdavron6@gmail.com>
2024-01-16 19:06:22 +0500

    fix in join tables

--- core/db.js
+++ core/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, { logging: true });
+const sequelize = new Sequelize(process.env.POSTGRES_URL, { logging: false });
 
 sequelize
   .authenticate()
