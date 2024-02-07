e63fd94440f8156322ff1edc10c1a7384929b131
davronkhamdamov <xamdamovdavron6@gmail.com>
2024-02-07 20:06:54 +0500

    is admi route added

--- router/admin.js
+++ router/admin.js
@@ -1,7 +1,11 @@
 const { Router } = require("express");
 const rout = Router();
 const Admin = require("../controller/admin");
+const AuthMiddleware = require("../middleware/auth");
 
 rout.post("/login", Admin);
+rout.get("/is-admin", AuthMiddleware, (req, res) => {
+    res.sendStatus(200);
+});
 
 module.exports = rout;
