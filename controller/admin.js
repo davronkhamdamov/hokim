278e18f95b9399eb2f1f6d7f3a0c3cda08305fec
davronkhamdamov <xamdamovdavron6@gmail.com>
2024-02-07 21:08:00 +0500

    set cookie removed

--- controller/admin.js
+++ controller/admin.js
@@ -5,17 +5,15 @@ const Admin = (req, res) => {
     if (login === process.env.LOGIN && password === process.env.PASSWORD) {
         const oneHour = 3600000;
         const expirationDate = new Date(Date.now() + oneHour);
-        res.cookie(
-            "authorization",
-            jwt.sign({ id: process.env.ACTION }, process.env.JWT_SECRET, {
-                expiresIn: "1h",
-            }),
-            {
-                expires: expirationDate,
-                secure: true,
-            }
-        );
         res.send({
+            token: jwt.sign(
+                { id: process.env.ACTION },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "1h",
+                }
+            ),
+            expirationDate,
             message: "Access granted",
         });
     } else {
