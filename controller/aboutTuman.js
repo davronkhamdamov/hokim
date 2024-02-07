8bbc491043d88cba1bd896ee0ac8de09c8d2d15d
davronkhamdamov <xamdamovdavron6@gmail.com>
2024-02-07 19:37:38 +0500

    fix type of database

--- controller/aboutTuman.js
+++ controller/aboutTuman.js
@@ -1,45 +1,45 @@
 const aboutTuman = require("../db/aboutTuman");
-aboutTuman.sync({ force: false });
+aboutTuman.sync({ force: true });
 const getAll = async (req, res) => {
-  try {
-    const allTuman = await aboutTuman.findAll();
-    res.send(allTuman);
-  } catch (err) {
-    res.send(err);
-  }
+    try {
+        const allTuman = await aboutTuman.findAll();
+        res.send(allTuman);
+    } catch (err) {
+        res.send(err);
+    }
 };
 const Create = async (req, res) => {
-  try {
-    await aboutTuman.create(req.body);
-    res.status(201).send({
-      message: "created",
-    });
-  } catch (err) {
-    res.send(err);
-  }
+    try {
+        await aboutTuman.create(req.body);
+        res.status(201).send({
+            message: "created",
+        });
+    } catch (err) {
+        res.send(err);
+    }
 };
 const Update = async (req, res) => {
-  try {
-    await aboutTuman.update(
-      { ...req.body, updated_at: new Date() },
-      {
-        where: { id: req.params.id },
-      },
-    );
-    res.status(204).send();
-  } catch (err) {
-    res.send(err);
-  }
+    try {
+        await aboutTuman.update(
+            { ...req.body, updated_at: new Date() },
+            {
+                where: { id: req.params.id },
+            }
+        );
+        res.status(204).send();
+    } catch (err) {
+        res.send(err);
+    }
 };
 const Delete = async (req, res) => {
-  try {
-    await aboutTuman.destroy({
-      where: { id: req.params.id },
-    });
-    res.status(204).send();
-  } catch (err) {
-    res.send(err);
-  }
+    try {
+        await aboutTuman.destroy({
+            where: { id: req.params.id },
+        });
+        res.status(204).send();
+    } catch (err) {
+        res.send(err);
+    }
 };
 
 module.exports = { getAll, Create, Update, Delete };
--- db/aboutTuman.js
+++ db/aboutTuman.js
@@ -2,116 +2,116 @@ const { DataTypes } = require("sequelize");
 const { sequelize } = require("../core/db");
 
 const AboutTuman = sequelize.define(
-  "aboutTuman",
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false,
-    },
-    tuman: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "Tuman is required",
+    "aboutTuman",
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
         },
-      },
-    },
-    maydon: {
-      type: Number,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "Yer maydoni is required",
+        tuman: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "Tuman is required",
+                },
+            },
         },
-      },
-    },
-    numberOfPeople: {
-      type: Number,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "Aholi soni required",
+        maydon: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "Yer maydoni is required",
+                },
+            },
         },
-      },
-    },
-    numberOfFemales: {
-      type: Number,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "Xotin-qizlar soni is required",
+        numberOfPeople: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "Aholi soni required",
+                },
+            },
         },
-      },
-    },
-    numberOfMans: {
-      type: Number,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "Erkaklar soni is required",
+        numberOfFemales: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "Xotin-qizlar soni is required",
+                },
+            },
         },
-      },
-    },
-    numberOfAdults: {
-      type: Number,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "Yoshlar soni is required",
+        numberOfMans: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "Erkaklar soni is required",
+                },
+            },
         },
-      },
-    },
-    numberOfBoys: {
-      type: Number,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "Bolalar soni is required",
+        numberOfAdults: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "Yoshlar soni is required",
+                },
+            },
         },
-      },
-    },
-    numberOfPupils: {
-      type: Number,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "O'quvchilar soni is required",
+        numberOfBoys: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "Bolalar soni is required",
+                },
+            },
         },
-      },
-    },
-    numberOfRetirees: {
-      type: Number,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "Nuroniylar soni is required",
+        numberOfPupils: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "O'quvchilar soni is required",
+                },
+            },
         },
-      },
-    },
-    numberOfDisabledPeople: {
-      type: Number,
-      allowNull: false,
-      validate: {
-        notNull: {
-          message: "Nogironlar soni is required",
+        numberOfRetirees: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "Nuroniylar soni is required",
+                },
+            },
         },
-      },
+        numberOfDisabledPeople: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    message: "Nogironlar soni is required",
+                },
+            },
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+        },
     },
-    updated_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-  },
-  {
-    timestamps: false,
-  },
+    {
+        timestamps: false,
+    }
 );
 
 module.exports = AboutTuman;
